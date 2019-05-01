d2a97f302c7266ce306d7d255b93b30d1e809b64
djday06 <dj.day061309@gmail.com>
2019-04-30 19:23:38 -0500

    Added detail page info.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../core/auth.service';
+import { AngularFireAuth } from '@angular/fire/auth';
 import { SessionService } from '../core/session.service';
 import { User } from '../core/user';
 import { Session } from '../core/session.model';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import {authState} from 'rxfire/auth';
 
 
@@ -12,21 +15,46 @@ import {authState} from 'rxfire/auth';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-  sessCollection;
-  samsUser2: User;
-  myMeetings: string[];
-  meeting: string;
+  myMeetings: Session[];
+  meetings: string[];
+  user: User;
+  sessions: Session[];
 
   constructor(public auth: AuthService, sessionService: SessionService) {
-    this.sessCollection = sessionService.sessionCollection;
-    console.log(`name: ${this.auth.user}`);
+    this.myMeetings = [];
+    this.user = this.auth.user.subscribe(user => {
+                                          this.user = user;
+                                          this.getInfo(sessionService);
+                                        }
+                                      );
+    }
 
+    getInfo(sessionService: SessionService) {
+      sessionService.getSessions().subscribe(sessions => {
+                                                this.sessions = sessions;
+                                                this.getMeetingData();
+                                              }
+                                            )
+    }
+
+   getMeetingData() {
+     this.meetings = this.user.meetings;
+     this.queryDBMeetingData();
+   }
+
+   queryDBMeetingData() {
+     for (let meeting of this.meetings) {
+       for(let session of this.sessions) {
+         if (session.id === meeting) {
+           this.myMeetings.push(session);
+         }
+       }
+     }
    }
 
 
   ngOnInit() {
-    // this.samsUser2 = this.auth.getUser();
-    // console.log(this.samsUser2);
+    
   }
 
 
