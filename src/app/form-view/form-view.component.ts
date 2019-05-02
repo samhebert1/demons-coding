b6e4269edc465c1d00d636df833e38b16f63b88a
djday06 <dj.day061309@gmail.com>
2019-05-02 14:47:52 -0500

    Added list of meeting information to edit page and added function to reload detail page after unenroll

--- src/app/form-view/form-view.component.ts
+++ src/app/form-view/form-view.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Session } from '../core/session.model';
+import { User } from '../core/user';
 import { SessionService } from '../core/session.service';
-import { AngularFirestoreCollection } from '@angular/fire/firestore';
+import { AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 
@@ -11,13 +12,22 @@ import { Observable } from 'rxjs';
   styleUrls: ['./form-view.component.css']
 })
 export class FormViewComponent implements OnInit {
-
-  constructor(public sessionService: SessionService, private route: ActivatedRoute) { }
-  sessions: Observable<Session[]>;
+  sessions: Session[];
   id: string;
-  session: Session;
-
+  session: Observable<Session>;
+  meeting: Session;
+  allUsers: User[];
+  enrolledHelpers: User[];
+  enrolledStudents: User[];
   model = { lessonname: '', meettime: null, helpers: [], students: [], numberHelpers: 0, numberStudents: 0 };
+
+  constructor(public sessionService: SessionService, private route: ActivatedRoute) {
+    this.enrolledHelpers = [];
+    this.enrolledStudents = [];
+  }
+
+
+
   userSubmit() {
     if (this.id === undefined){
       this.addSession(this.model);
@@ -40,11 +50,55 @@ export class FormViewComponent implements OnInit {
     this.sessionService.updateSession(session, this.id);
   }
 
+  getSession() {
+    for (let session of this.sessions) {
+      if (session.id === this.id) {
+        return session;
+      }
+    }
+  }
+
+  filterUsers() {
+    //Loops through all users
+    for (let user of this.allUsers) {
+      //Loops through helpers in meeting.
+      for (let helper of this.meeting.helpers) {
+        if (user.uid === helper) {
+          this.enrolledHelpers.push(user);
+        } //end if
+      } //end inner for
+      //Loops through students in meeting.
+      for (let student of this.meeting.students) {
+        if (user.uid === student) {
+          this.enrolledStudents.push(user);
+        } //end if
+      } //end inner for
+    } //end outer for
+  }
+
+  getEnrolledUsers() {
+    this.sessionService.getUsers().subscribe(users => {
+      this.allUsers = users;
+      this.filterUsers();
+    })
+  }
+
   ngOnInit() {
+    //Gets meeting id from url
     this.route.params.subscribe(params => {
     this.id = params['id'];
     });
 
+    this.sessionService.getSessions().subscribe(sessions => {
+                                                this.sessions = sessions;
+                                                this.meeting = this.getSession();
+                                                this.getEnrolledUsers();
+                                              });
 
+    this.model = { lessonname: this.meeting.lessonname, meettime: this.meeting.meettime,
+                  helpers: this.meeting.helpers, students: this.meeting.students,
+                  numberHelpers: this.meeting.numberHelpers, numberStudents: this.meeting.numberStudents };
     }
+
+
   }
